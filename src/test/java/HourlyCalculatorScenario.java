a0c3685da4315cc0799de4514cf82ed214e79dfa
agorb <anna.gorb@gmail.com>
2016-08-15 13:36:49 +0300

    Commit and push

--- src/test/java/HourlyCalculatorScenario.java
+++ src/test/java/HourlyCalculatorScenario.java
@@ -1,5 +1,7 @@
+import com.thoughtworks.selenium.condition.Not;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -7,26 +9,32 @@ import org.testng.annotations.Test;
  */
 public class HourlyCalculatorScenario {
 
-    @Test (groups = {"P1","regression"})
+    @DataProvider(name = "hourlyData")
+    public static Object[][] hourlyData() {
+        return new Object[][]{{"50","160","80","8","8640"},{"40","150","70","5","6350"}};
 
-    public void hourly()throws Exception {
+    }
+
+    @Test (groups = {"P1","regression"} , dataProvider = "hourlyData")
+
+    public void hourly(String payRate, String hours, String payRate1, String hours1, String result)throws Exception {
 
         StartTest.driver.get("http://www.paycheckcity.com/calculator/hourly/");
-        StartTest.driver.findElement(By.id("addRate")).click();
-     /*   if (Assert.assertFalse(StartTest.driver.))
-        {StartTest.driver.findElement(By.id("addRate")).click();}*/
+        /*StartTest.driver.findElement(By.id("addRate")).click();*/
+        if (StartTest.driver.findElements(By.id("calcType.rates1.payRate")).isEmpty())
+                {StartTest.driver.findElement(By.id("addRate")).click();}
         StartTest.driver.findElement(By.id("calcType.rates0.payRate")).clear();
-        StartTest.driver.findElement(By.id("calcType.rates0.payRate")).sendKeys("50");
+        StartTest.driver.findElement(By.id("calcType.rates0.payRate")).sendKeys(payRate);
         StartTest.driver.findElement(By.id("calcType.rates0.hours")).clear();
-        StartTest.driver.findElement(By.id("calcType.rates0.hours")).sendKeys("160");
+        StartTest.driver.findElement(By.id("calcType.rates0.hours")).sendKeys(hours);
         StartTest.driver.findElement(By.id("calcType.rates1.payRate")).clear();
-        StartTest.driver.findElement(By.id("calcType.rates1.payRate")).sendKeys("80");
+        StartTest.driver.findElement(By.id("calcType.rates1.payRate")).sendKeys(payRate1);
         StartTest.driver.findElement(By.id("calcType.rates1.hours")).clear();
-        StartTest.driver.findElement(By.id("calcType.rates1.hours")).sendKeys("8");
+        StartTest.driver.findElement(By.id("calcType.rates1.hours")).sendKeys(hours1);
         StartTest.driver.findElement(By.id("generalInformation.grossPayAmount")).click();
 
 
-        Assert.assertEquals(StartTest.driver.findElement(By.id("generalInformation.grossPayAmount")).getAttribute("value"),"8640");
+        Assert.assertEquals(StartTest.driver.findElement(By.id("generalInformation.grossPayAmount")).getAttribute("value"), result);
 
     }
 }
